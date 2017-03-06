8a62f36b33307256b859def214dabfee60c7d1fd
Emilio Rinaudo <Emilio Rinaudo>
2017-03-06 23:17:39 +0100

    prima struttura

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+var firstName = "Jeff";
+var secondName = "Green";
+var cars = new Array();
+
+
+
+
+/* if(firstName == "John")
+	{
+		alert("Si sono io");
+	}
+else if (firstName == "Steve")
+{
+	alert("I'm not Jhon, I'm Steve")
+}
+else
+	{
+		alert("Non sono io");
+	} */
+/* cars[0] = "Boh";
+cars[1] = "Test";
+cars[2] = "Bu"; */
+
+cars = ["Honda", "Tool", "Tesla"];
+
+for (var i = 0; i < cars.length; i++) {
+	console.log(cars[i]);
+}
+function sayHi(){
+	alert("Hi");
+}
+function sayIt(word) {
+	alert(word);
+	
+}
+
+sayHi();
+sayIt("lol");
